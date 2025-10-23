242e4df9c899c181b167ee883549ffec22cad697
Muhammed Minhaj VP <vpmuhammedminhaj02@gmail.com>
2025-10-23 22:23:02 +0530

    min-rip fix

--- minrip.ps1
+++ minrip.ps1
@@ -18,7 +18,8 @@ function Start-Rip {
 
     Write-Host "Starting MIN-RIP..."
 
-    ffmpeg.exe -i "$fileName" -map 0:v:0 -vf $scale -c:v libx265 -preset $preset -x265-params $crf -pix_fmt yuv420p10le -map $audio -c:a aac -b:a 192k -ac 2 ".\output.mkv"
+    ffmpeg.exe -i "$fileName" -map 0:v:0 -vf $scale -c:v libx265 -preset $preset `
+        -x265-params $crf -pix_fmt yuv420p10le -map $audio -c:a aac -b:a 192k -ac 2 ".\output.mkv"
 }
 
 function Option {
@@ -45,12 +46,32 @@ if ([string]::IsNullOrWhiteSpace($fileName)) {
 
     $fileName = Read-Host "Enter the input video file name"
 
-    if (Option("HQ(Y/N)")) {
-        Write-Host "High Quality option selected."
-    
-        $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
-    
-        Start-Rip -fileName "$fileName" -res $res -preset "slow" -quality "22" -audioChannel "0"
+    if (Option("Use GPU(Y/N)")) {
+        Write-Host "Encoding with GPU"
+        $res = Read-Host "Enter Horizontal Resolution (e.g., 3840, 1920, 1280)"
+        $crop = Read-Host "Want to crop? 1920:800:0:140"
+        $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
+        $audioChannel = Read-Host "Enter the audio stream"
+        $scale = ""
+
+        if ([string]::IsNullOrWhiteSpace($crop)) {
+            $scale = "scale=${res}:-2,format=yuv420p"
+        }
+        else {
+            $scale = "crop=${crop},scale=${res}:-2,format=yuv420p"
+        }
+
+        $audio = "0:a:" + $audioChannel
+
+        Clear-Host
+        Write-Host "Starting MIN-RIP (8-bit, CPU scale, explicit maps)..."
+
+        ffmpeg.exe -i "$fileName"  -map 0:v:0 -map "$audio" -vf "$scale" `
+            -c:v hevc_nvenc -preset p7 -rc vbr -cq $quality -tune hq -multipass fullres `
+            -spatial-aq 1 -temporal-aq 1 -aq-strength 8 -rc-lookahead 32 `
+            -c:a libopus -b:a 160k -ac 2 ".\output_nvenc_${quality}_8bit.mkv"
+
+
     }
     else {
         $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
@@ -64,38 +85,35 @@ if ([string]::IsNullOrWhiteSpace($fileName)) {
 }
 elseif (Option("Use GPU(Y/N)")) {
     Write-Host "Encoding with GPU"
-    $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
+    $res = Read-Host "Enter Horizontal Resolution (e.g., 3840, 1920, 1280)"
+    $crop = Read-Host "Want to crop? 1920:800:0:140"
+    $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
     $audioChannel = Read-Host "Enter the audio stream"
+    
+    $scale = ""
 
-    $scale = "scale=" + $res + ":-2"  
-    $audio = "0:a:" + $audioChannel
-
-    ffmpeg.exe -i "$fileName" -map 0:v:0 -vf "$scale" -c:v hevc_nvenc -preset p7 -rc vbr_hq -cq 22 -map "$audio" -c:a aac -b:a 192k -ac 2 ".\output_gpu.mkv"
-}
-elseif (Option("Use iGPU(Y/N)")) {
-    Write-Host "Encoding with iGPU"
-    $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
-    $audioChannel = Read-Host "Enter the audio stream"
-
-    $scale = "scale=" + $res + ":-2"  
-    $audio = "0:a:" + $audioChannel
-
-    ffmpeg.exe -hwaccel qsv -i "$fileName" -map 0:v:0 -vf "$scale" -c:v hevc_qsv -global_quality 22 -map "$audio" -c:a aac -b:a 192k -ac 2 ".\output_igpu.mkv"
-}
-else {
-    if (Option("HQ(Y/N)")) {
-        Write-Host "High Quality option selected."
-
-        $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
-
-        Start-Rip -fileName "$fileName" -res $res -preset "slow" -quality "22" -audioChannel "0"
+    if ([string]::IsNullOrWhiteSpace($crop)) {
+        $scale = "scale=${res}:-2,format=yuv420p"
     }
     else {
-        $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
-        $preset = Read-Host "Enter Preset (ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow)"
-        $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
-        $audioChannel = Read-Host "Enter the audio stream"
-
-        Start-Rip -fileName "$fileName" -res $res -preset $preset -quality $quality -audioChannel $audioChannel
+        $scale = "crop=${crop},scale=${res}:-2,format=yuv420p"
     }
+
+    $audio = "0:a:" + $audioChannel
+
+    Clear-Host
+    Write-Host "Starting MIN-RIP (8-bit, CPU scale, explicit maps)..."
+
+    ffmpeg.exe -i "$fileName" -map 0:v:0 -map "$audio" -vf "$scale" `
+        -c:v hevc_nvenc -preset p7 -rc vbr -cq $quality -tune hq -multipass fullres `
+        -spatial-aq 1 -temporal-aq 1 -aq-strength 8 -rc-lookahead 32 `
+        -c:a libopus -b:a 160k -ac 2 ".\output_nvenc_${quality}_8bit.mkv"
+}
+else {   
+    $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
+    $preset = Read-Host "Enter Preset (ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow)"
+    $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
+    $audioChannel = Read-Host "Enter the audio stream"
+
+    Start-Rip -fileName "$fileName" -res $res -preset $preset -quality $quality -audioChannel $audioChannel  
 }
