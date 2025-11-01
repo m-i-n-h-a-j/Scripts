bef1d2f8b6c94ae9f8bb1d028f060285e0cd4caa
Muhammed Minhaj VP <vpmuhammedminhaj02@gmail.com>
2025-11-01 20:09:24 +0530

    major code change, added av1

--- minrip.ps1
+++ minrip.ps1
@@ -1,27 +1,72 @@
 param (
     [string]$fileName = ""
 )
-function Start-Rip {
+function Start-CpuRip {
     param (
         [string]$fileName,
-        [string]$res,
+        [string]$scale,
+        [string]$start,
+        [string]$end,
+        [string]$title,
         [string]$preset,
         [string]$quality,
-        [string]$audioChannel
+        [string]$audio
     )
 
-    $scale = "scale=" + $res + ":-2"
     $crf = "crf=" + $quality + ":aq-mode=3"
-    $audio = "0:a:" + $audioChannel
 
-    Clear-Host
+    Write-Host "Starting MIN-RIP (SW)..."
 
-    Write-Host "Starting MIN-RIP..."
-
-    ffmpeg.exe -i "$fileName" -map 0:v:0 -vf $scale -c:v libx265 -preset $preset `
-        -x265-params $crf -pix_fmt yuv420p10le -map $audio -c:a libopus -b:a 128k `
-        -vbr on -application audio -ar 48000 -ac 2  ".\output.mkv"
+    ffmpeg.exe -hide_banner -i "$fileName" -ss $start -to $end -map 0:v:0 `
+        -vf $scale -c:v libx265 -preset $preset -x265-params $crf `
+        -pix_fmt yuv420p10le -map $audio -c:a libopus -b:a 128k `
+        -vbr on -application audio -ar 48000 -ac 2 `
+        -metadata title="$title" -metadata:s:v title="HEVC-10bit" `
+        -metadata:s:a title="OPUS-2CH - VBR(128kbps)" ".\cpu_${title}__${preset}__${quality}_output.mkv"
 }
+
+
+function Start-CpuRipAv1 {
+    param (
+        [string]$fileName,
+        [string]$scale,
+        [string]$start,
+        [string]$end,
+        [string]$title,
+        [string]$quality,
+        [string]$audio
+    )
+
+    Write-Host "Starting MIN-RIP (SW - AV1)..."
+
+    ffmpeg -hide_banner -i "$fileName" -ss $start -to $end `
+        -map 0:v:0 -c:v libsvtav1 -vf "$scale" -pix_fmt yuv420p10le `
+        -preset -2 -crf $quality -map $audio -c:a libopus -b:a 128k `
+        -vbr on -application audio -ar 48000 -ac 2 ".\av1_${title}__${preset}__${quality}_output.mkv"
+}
+
+function Start-GpuRip {
+    param (
+        [string]$fileName,
+        [string]$scale,
+        [string]$start,
+        [string]$end,
+        [string]$title,
+        [string]$quality,
+        [string]$audio
+    )
+    
+    Write-Host "Starting MIN-RIP (HW)..."
+
+    ffmpeg.exe -hide_banner -i "$fileName" -ss $start -to $end -c:v hevc_nvenc -map 0:v:0 `
+        -map "$audio" -vf "$scale" -preset p7 -tune uhq -profile:v main10 -pix_fmt p010le `
+        -rc vbr -cq $quality -b:v 0 -rc-lookahead 32 -lookahead_level auto -spatial_aq 1 `
+        -temporal_aq 1 -aq-strength 8 -b_ref_mode each -unidir_b 0 -c:a libopus -b:a 128k `
+        -vbr on -application audio -ar 48000 -metadata title="$title" `
+        -metadata:s:v title="HEVC-10bit" -metadata:s:a title="OPUS-2CH - VBR(128kbps)" `
+        -ac 2 ".\nvenc_${title}_${quality}_10bit.mkv"
+
+} 
 
 function Option {
     param (
@@ -44,89 +89,51 @@ function Option {
 Clear-Host
 
 if ([string]::IsNullOrWhiteSpace($fileName)) {
-
     $fileName = Read-Host "Enter the input video file name"
+}
+$scaleG = ""
+$resG = Read-Host "Enter Resolution (e.g., 3840, 1920, 1280)"
+$cropG = Read-Host "Want to crop? (e.g., 1920:800:0:140)"
 
-    if (Option("Use GPU(Y/N)")) {
-        Write-Host "Encoding with GPU"
-        $res = Read-Host "Enter Horizontal Resolution (e.g., 3840, 1920, 1280)"
-        $crop = Read-Host "Want to crop? 1920:800:0:140"
-        $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
-        $audioChannel = Read-Host "Enter the audio stream"
-        $start = Read-Host "Start at"
-        $end = Read-Host "End at"
-        $title = Read-Host "File name"
-
-        $scale = ""
-
-        if ([string]::IsNullOrWhiteSpace($crop)) {
-            $scale = "scale=${res}:-2"
-        }
-        else {
-            $scale = "crop=${crop},scale=${res}:-2"
-        }
-
-        $audio = "0:a:" + $audioChannel
-
-        Clear-Host
-        Write-Host "Starting MIN-RIP (GPU)..."
-
-        ffmpeg.exe -hide_banner -i "$fileName" -ss $start -to $end -c:v hevc_nvenc -map 0:v:0 -map "$audio" -vf "$scale" `
-            -preset p7 -tune uhq -profile:v main10 -pix_fmt p010le `
-            -rc vbr -cq $quality -b:v 0 -rc-lookahead 32 -lookahead_level auto -spatial_aq 1 `
-            -temporal_aq 1 -aq-strength 8 -b_ref_mode each -unidir_b 0 -c:a libopus -b:a 128k `
-            -vbr on -application audio -ar 48000 -metadata title="$title" -metadata:s:v title="HEVC-10bit" -metadata:s:a title="OPUS-2CH@128kbps" `
-            -ac 2 ".\nvenc_${quality}_10bit.mkv"
-
-
+if (Option("Is Horizontal Resolution(Y/N))")) {
+    if ([string]::IsNullOrWhiteSpace($cropG)) {
+        $scaleG = "scale=${resG}:-2"
     }
     else {
-        $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
-        $preset = Read-Host "Enter Preset (ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow)"
-        $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
-        $audioChannel = Read-Host "Enter the audio stream"
-    
-    
-        Start-Rip -fileName "$fileName" -res $res -preset $preset -quality $quality -audioChannel $audioChannel
+        $scaleG = "crop=${cropG},scale=${resG}:-2"
     }
 }
-elseif (Option("Use GPU(Y/N)")) {
-    Write-Host "Encoding with GPU"
-    $res = Read-Host "Enter Horizontal Resolution (e.g., 3840, 1920, 1280)"
-    $crop = Read-Host "Want to crop? 1920:800:0:140"
-    $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
-    $audioChannel = Read-Host "Enter the audio stream"
-    $start = Read-Host "Start at"
-    $end = Read-Host "End at"
-    $title = Read-Host "File name"
-
-    
-    $scale = ""
-
-    if ([string]::IsNullOrWhiteSpace($crop)) {
-        $scale = "scale=${res}:-2"
+else {
+    if ([string]::IsNullOrWhiteSpace($cropG)) {
+        $scaleG = "scale=-2:${resG}"
     }
     else {
-        $scale = "crop=${crop},scale=${res}:-2"
+        $scaleG = "crop=${cropG},scale=-2:${resG}"
     }
-
-    $audio = "0:a:" + $audioChannel
-
-    Clear-Host
-    Write-Host "Starting MIN-RIP (GPU)..."
-
-    ffmpeg.exe -hide_banner -i "$fileName" -ss $start -to $end -c:v hevc_nvenc -map 0:v:0 -map "$audio" -vf "$scale" `
-        -preset p7 -tune uhq -profile:v main10 -pix_fmt p010le `
-        -rc vbr -cq $quality -b:v 0 -rc-lookahead 32 -lookahead_level auto -spatial_aq 1 `
-        -temporal_aq 1 -aq-strength 8 -b_ref_mode each -unidir_b 0 -c:a libopus -b:a 128k `
-        -vbr on -application audio -ar 48000 -metadata title="$title" -metadata:s:v title="HEVC-10bit" -metadata:s:a title="OPUS-2CH@128kbps" `
-        -ac 2 ".\nvenc_${quality}_10bit.mkv"
 }
-else {   
-    $res = Read-Host "Enter Horizontal Resolution (e.g., 1920 or 1280)"
+
+$qualityG = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
+$audioChannelG = Read-Host "Enter the audio stream"
+$startG = Read-Host "Start at"
+$endG = Read-Host "End at"
+$titleG = Read-Host "File name"
+$audioG = "0:a:" + $audioChannelG
+
+
+if (Option("Use GPU(Y/N)")) {
+    Start-GpuRip -fileName "$fileName" -scale "$scaleG" `
+        -start $startG -end $endG -title $titleG `
+        -quality $qualityG -audio $audioG
+}
+elseif (Option("AV1 (Y/N)")) {
+    Start-CpuRipAv1 -fileName "$fileName" -scale "$scaleG" `
+        -start $startG -end $endG -title $titleG `
+        -quality $qualityG -audio $audioG
+}
+else {
     $preset = Read-Host "Enter Preset (ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow)"
-    $quality = Read-Host "Enter Quality (18, 20, 22, 24, 26, 28)"
-    $audioChannel = Read-Host "Enter the audio stream"
-
-    Start-Rip -fileName "$fileName" -res $res -preset $preset -quality $quality -audioChannel $audioChannel  
+  
+    Start-CpuRip -fileName "$fileName" -scale $scaleG `
+        -start $startG -end $endG -title $titleG -preset $preset `
+        -quality $qualityG -audio $audioG
 }
