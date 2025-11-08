5e0ff495dc66865370d3232ddd12254e9070985e
Muhammed Minhaj VP <vpmuhammedminhaj02@gmail.com>
2025-11-08 19:59:16 +0530

    flac to opus ripper

---
+++ audmin.ps1
@@ -0,0 +1,10 @@
+Get-ChildItem -Filter *.flac | ForEach-Object {
+    $inputFile = $_.FullName
+    $base = [System.IO.Path]::GetFileNameWithoutExtension($inputFile)
+    $outputName = "$base.opus"
+
+    Write-Host "Converting $inputFile → $output"
+
+    ffmpeg -i "$inputFile" -ar 48000 -ac 2 -c:a libopus -b:a 160k `
+        -vbr on -application audio -map_metadata 0 "$outputName" -y
+}
