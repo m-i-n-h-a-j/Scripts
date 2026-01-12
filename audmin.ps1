0bc602461f830c361b6a89c4d83aa0b98b205655
Muhammed Minhaj VP <vpmuhammedminhaj02@gmail.com>
2026-01-12 11:01:43 +0530

    audmin ffmpeg command improvements

--- audmin.ps1
+++ audmin.ps1
@@ -3,9 +3,11 @@ Get-ChildItem -Filter *.flac | ForEach-Object {
     $base = [System.IO.Path]::GetFileNameWithoutExtension($inputFile)
     $outputName = "$base.opus"
 
-    Write-Host "Converting $inputFile → $output"
+    Write-Host "Converting $inputFile → $outputName"
 
-    ffmpeg -i "$inputFile" -ar 48000 -ac 2 -c:a libopus `
-        -b:a 192k -compression_level 10 -frame_duration 20 `
-        -vbr on -application audio -map_metadata 0 "$outputName" -y
+    ffmpeg -i "$inputFile" -map 0:a -map 0:v? `
+        -c:v copy -ar 48000 -ac 2 -c:a libopus `
+        -b:a 192k -compression_level 10 `
+        -vbr on -application audio `
+        -map_metadata 0 "$outputName" -y
 }
